3ed26830a6ed25ea2a056a964fe9acf1461c4fe9
onslaught7 <krutibashmohapatra7@gmail.com>
2026-02-07 17:05:23 +0530

    add the gateway main.go file and update the gateway code to push to redis

---
+++ gateway/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/redis/go-redis/v9"
+)
+
+
+// Define the Data Contract we expect from the user
+type IngestRequest struct {
+	UserID string `json:"user_id"`
+	DocumentID string `json:"document_id"`
+	Content string `json:"content"`
+}
+
+var ctx = context.Background()
+
+func main() {
+	redisAddr := os.Getenv("REDIS_ADDR")
+	if redisAddr == "" {
+		redisAddr = "localhost:6379" // Default for local testing
+	}
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: redisAddr,
+	})
+
+	// Test connection
+	_, err := rdb.Ping(ctx).Result()
+	if err != nil {
+		log.Fatalf("Could not connect to Redis: %v", err)
+	}
+	
+	app := fiber.New()
+
+	app.Post("/ingest", func(c *fiber.Ctx) error {
+		// Parse JSON
+		payload := new(IngestRequest)
+		if err := c.BodyParser(payload); err != nil {
+			return c.Status(500).JSON(fiber.Map{"error": "Redis Queue Failed"})
+		}
+
+		return c.Status(202).JSON(fiber.Map{
+			"status": "accepted",
+			"message": "Document Queued for Processing",
+			"data": payload.DocumentID,
+		})
+	})
+
+	log.Fatal(app.Listen(":8080"))
+}
